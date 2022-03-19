e3f46e5984fe30e1babbd1a423b5ca9cb0991339
jasonjohnson47 <jasonjohnson47@gmail.com>
2022-03-19 12:57:37 -0400

    Start setting up GameSetupComponent as reactive form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ReactiveFormsModule } from '@angular/forms';
 import { FloatingLabelInputComponent } from './floating-label-input/floating-label-input.component';
 import { GameSetupComponent } from './game-setup/game-setup.component';
 
@@ -14,7 +15,8 @@ import { GameSetupComponent } from './game-setup/game-setup.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/game-setup/game-setup.component.ts
+++ src/app/game-setup/game-setup.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray, FormGroup, FormControl } from '@angular/forms';
 import { PlayerService } from '../player.service';
 import { Player } from '../player';
  
@@ -9,16 +10,44 @@ import { Player } from '../player';
 })
 export class GameSetupComponent implements OnInit {
 
-  constructor(private playerService: PlayerService) { }
+  constructor(
+    private playerService: PlayerService,
+    private fb: FormBuilder
+  ) { }
 
   roundToEdit = 1;
+
   players: Player[] = [];
+  playerNameControls: FormControl[] = [];
+  playerNamesForm: FormGroup = this.fb.group({});
 
   getPlayers(): void {
     this.players = this.playerService.getPlayers();
+
+    this.playerNameControls = this.players.map((player: Player) => this.fb.control(player.name));
+
+    this.playerNamesForm = this.fb.group({
+      playerNames: this.fb.array(
+        this.playerNameControls
+      )
+    });
   }
 
-  addPlayer() {}
+  get playerNames() {
+    return this.playerNamesForm.get('playerNames') as FormArray;
+  }
+
+  addPlayerNameField() {
+    this.playerNames.push(this.fb.control(''));
+  }
+
+  onSubmit() {
+    console.log(this.playerNames.value);
+    console.log(this.players);
+    console.log(this.playerNameControls);
+    //startGame()
+    //continueGame()
+  }
   startGame() {}
   continueGame() {}
 
